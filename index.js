9e34abdb1caa68e663d6e97ceac3a2f059f488d7
shaporjur <shaporjur@alum.us.es>
2024-02-16 11:57:49 +0100

    L04

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 let cool = require("cool-ascii-faces");
 
-console.log(cool());
+let express = require("express");
+
+let app = express();
+
+//Si es constante y además para no etenr que cambiarlo en cada lado
+const PORT = process.env.PORT || 10000; //Así es si la creas en terminal (L04), sino esta definido le da el otro valor
+
+//Deveulve esto si pone en navegador localhost:10000/...
+app.get("/hola", (req,res) => {
+    res.send(`<html> <body> <h1> HOLA </h1> </body> </html>`)
+});
+app.get("/cool", (req,res) => {
+    res.send(`<html> <body> <h1> HOLA  ${cool()}</h1> </body> </html>`)
+});
+
+//Así mejor
+app.listen(PORT, () =>
+{
+    console.log(`Server listening on port PORT ${PORT} `  ); //Asincrono
+});
+
+
+console.log(`Server initializing...`) //Sincrono 
+                                      //Esto se ejecuta antes que lo anterior porque el del PORT tiene que escuchar entonces va a tardar un poquito más,
+                                      // pero el código sigue ejecutandi por eso esto sale primero
